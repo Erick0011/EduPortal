04b4a4206c78547c0b727d0a8998895abb98cc86
Erick0011 <erick.baptista00@gmail.com>
2025-02-25 03:03:18 +0100

    Some changes

--- app.py
+++ app.py
@@ -196,6 +196,7 @@ class Inscricao(db.Model):
     status = db.Column(db.String(20), nullable=False, default='Pendente')
     data_inscricao = db.Column(db.DateTime, default=datetime.utcnow)
     data_atualizacao = db.Column(db.DateTime, default=datetime.utcnow, onupdate=datetime.utcnow, nullable=False)
+    mensagem_instituicao = db.Column(db.Text, nullable=True)
 
     # Relacionamentos
     aluno = db.relationship('Aluno', backref='inscricoes_rel', lazy=True)
